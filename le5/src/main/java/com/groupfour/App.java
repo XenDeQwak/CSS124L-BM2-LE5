0f7362446bfc43fd79e417dd35af1a7f5bbe8bae
XenDQ <xeanxyrone@gmail.com>
2024-10-03 17:52:26 +0800

    initial commit

---
+++ le5/src/main/java/com/groupfour/App.java
@@ -0,0 +1,28 @@
+package com.groupfour;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        VBox root = new VBox();
+        scene = new Scene(root, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
