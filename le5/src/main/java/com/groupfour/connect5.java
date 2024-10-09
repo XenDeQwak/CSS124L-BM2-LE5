52239efc55eb453af5bf212f848480584a516860
Azyur-i <107308649+Azyur-i@users.noreply.github.com>
2024-10-09 13:54:32 +0800

    updated directories of connect5

--- le5/src/main/java/com/groupfour/connect5.java
+++ le5/src/main/java/com/groupfour/connect5.java
@@ -4,28 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
+
 /**
  * JavaFX App
  */
-public class connect5 extends Application {
+public class connect5 {
 
     private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
+    public connect5() throws IOException {
+    App.getStage().close();
 
-        Controller musicController = new Controller();
+        Stage c5Stage = new Stage();
+        controllerC5 musicController = new controllerC5();
         musicController.playBackgroundMusic();
         scene = new Scene(loadFXML("connect5"), 1280, 750);
-        stage.setScene(scene);
-        stage.setTitle("Connect 5: Bonanza");
-        stage.getIcons().add(new Image("file:src\\main\\res\\c5Logo.png"));
-        stage.show();
+        c5Stage.setScene(scene);
+        c5Stage.setTitle("Connect 5: Bonanza");
+        c5Stage.getIcons().add(new Image("file:le5\\src\\main\\resources\\com\\groupfour\\res\\c5Logo.png"));
+        c5Stage.show();
     }
-
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
@@ -34,9 +36,4 @@ public class connect5 extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(connect5.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
