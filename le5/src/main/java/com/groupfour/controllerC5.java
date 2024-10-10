6561f208dee6ca84c4fad561b9894057827fe36f
Azyur-i <107308649+Azyur-i@users.noreply.github.com>
2024-10-10 17:58:44 +0800

    connect5 FINAL controller update

--- le5/src/main/java/com/groupfour/controllerC5.java
+++ le5/src/main/java/com/groupfour/controllerC5.java
@@ -1,8 +1,11 @@
 package com.groupfour;
 import javafx.scene.control.Slider;
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.fxml.FXML;
+
+import java.io.File;
 import java.net.URL;
 
 public class controllerC5{
@@ -23,8 +26,21 @@ public class controllerC5{
         Media media = new Media(resource.toExternalForm());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setVolume(0.5);
         mediaPlayer.play();
     }
+
+    public void playSoundEffect() {
+        String soundFile = "click.mp3";
+        URL resource = getClass().getResource(soundFile);
+        if (resource == null) {
+            System.out.println("Error: Could not find music file " + soundFile);
+            return;
+        }
+        AudioClip sound = new AudioClip(resource.toExternalForm());
+        sound.setVolume(1.0);
+        sound.play();
+}
 
     public void stopBackgroundMusic() {
         mediaPlayer.stop();
